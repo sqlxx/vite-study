1e3715870bb35cbc836a72cc19283cd9a05ee44d
Sun Qin <sunqin@maycur.com>
2022-06-07 20:33:13 +0800

    temp commit

--- src/pages/workspace/index.tsx
+++ src/pages/workspace/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from 'styled-components';
 import "./index.scss"
 import ScrollableContext from "./ScrollableContent";
@@ -7,10 +7,19 @@ const WorkspaceDiv = styled.div`
     background-color: #f9f9f9;
 `;
 
+
 const Workspace = () => {
 
+    const [statusText, setStateText] = useState<string>('initial');
+
+    const handleClickWorkspace = React.useCallback((e) => {
+    
+        setStateText(JSON.stringify(e));
+    }, []);
+
     return (
-        <WorkspaceDiv className="workspace-container">
+        <WorkspaceDiv className="workspace-container" onClick={handleClickWorkspace}>
+            <div>status is: {statusText}</div>
             <ScrollableContext></ScrollableContext>
         </WorkspaceDiv>
     )
