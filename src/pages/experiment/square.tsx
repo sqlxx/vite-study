0fcf3d5c982279b29cc0fffc092986b3783c942b
Sun Qin <sqlxx@163.com>
2022-08-16 16:11:00 +0800

    change style

--- src/pages/experiment/square.tsx
+++ src/pages/experiment/square.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement, ReactNode } from 'react'
 export default function Square({ black, children} : {black: boolean, children?: ReactNode} ) {
     const fill:string = black ? 'black' : 'white'
     const stroke:string = black ? 'white' : 'black'
-    return (<div style={{ backgroundColor: fill, color: stroke, width: '100%', height: '100%'}} >
+    return (<div style={{ backgroundColor: fill, color: stroke, width: '100%', height: '100%', textAlign: 'center'}} >
             { children }
         </div>)
 }
